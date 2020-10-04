67433855884620c46818ea5edec9efd035f6544e
WilbertBocanegra <40700680+WilbertBocanegra@users.noreply.github.com>
2020-10-03 22:38:18 -0500

    update template

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-const express = require('express');
-const { ApolloServer, PubSub, gql } = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
 const { mongoConnect, schema, postgresConnect } = require('./utils/app');
 
-const app = express();
 const port = 3000;
 
 mongoConnect().then(success => {
@@ -17,14 +15,7 @@ postgresConnect().then(success => {
     console.log(err);
 });
 
-const pubsub = new PubSub();
 const server = new ApolloServer({ schema: schema });
-//server.applyMiddleware({app})
 
-/*
-app.listen(port, (url) => {
-    console.log(`🚀 Server ready at ${port}`);
-});*/
-
-server.listen(port).then(({ url }) => console.log(`server started at ${url}`));
+server.listen(port).then(({ url }) => console.log(`🚀 Server ready at ${port}`));
 
